69478b0b31c0f88bf04431d66d8902a96c9e91f3
shhuubb <150588045+shhuubb@users.noreply.github.com>
2024-12-08 23:04:12 +0300

    Update AdditTask2.py

--- Informatic (1-2 sem)/Lab4/AdditTask2.py
+++ Informatic (1-2 sem)/Lab4/AdditTask2.py
@@ -2,11 +2,8 @@ import re
 with open("file_inp.xml", "r") as file_inp:
     main_string = file_inp.read()[39:]
 
-
-
 main_string = re.sub(r'<(\w+)>', r"\1: ", main_string)
 main_string = re.sub(r'(\n)?\s*</\w+>', '', main_string)
-
 main_string = re.sub(r'    ', r'  ', main_string)
 main_string = re.sub(r"    <(\w+) (\w+)\s*=\s*'(\w+)'>", r"  - \1: \n      '@\2': \3", main_string)
 
