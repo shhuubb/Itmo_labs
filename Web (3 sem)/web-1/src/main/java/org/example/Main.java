261525274edd632cb31acb7dfb302af6d759f76e
Egor <egorshubin055@gmail.com>
2025-09-11 11:17:19 +0300

    Add web dir

---
+++ Web (3 sem)/web-1/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+import com.fastcgi.FCGIInterface;
+
+import java.io.File;
+
+
+class Main {
+    public static void main (String args[]) {
+        File file = new File("input.txt");
+        String s =
+    }
+}
